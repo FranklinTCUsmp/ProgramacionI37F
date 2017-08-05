d073ebf206c15c1eb759a1f915ac274fda6d1c7f
Jonathan Chavez <jonathan.chavez@outlook.com>
2017-08-05 18:19:42 -0500

    Creado ejercicio 2 con .NET (app consola)

---
+++ Semana 02/PRYEjercicio2/PRYEjercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PRYEjercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (true)
+            {
+                Console.WriteLine("Hola papu");
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
